0eb1b5c2450d93682309433a8daeea2a3a21a006
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2023-10-08 21:26:55 +0530

    ui: landing page

---
+++ src/assets/index.ts
@@ -0,0 +1,27 @@
+import logo from "@assets/logo.png";
+import hexagonGradient from "@assets/hexagon-gradient.svg";
+import partner1 from "@assets/partner1.svg";
+import partner2 from "@assets/partner2.svg";
+import partner3 from "@assets/partner3.svg";
+import partner4 from "@assets/partner4.svg";
+import partner5 from "@assets/partner5.svg";
+import partner6 from "@assets/partner6.svg";
+import partner7 from "@assets/partner7.svg";
+import partner8 from "@assets/partner8.svg";
+import partner9 from "@assets/partner9.svg";
+import partner10 from "@assets/partner10.svg";
+
+export {
+  logo,
+  hexagonGradient,
+  partner1,
+  partner2,
+  partner3,
+  partner4,
+  partner5,
+  partner6,
+  partner7,
+  partner8,
+  partner9,
+  partner10,
+};
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,15 +1,21 @@
-import { useAuth } from "@/context/AuthContext";
+import { PrimaryBtn } from "@/app/page";
+import { logo } from "../../assets/index";
+import Image from "next/image";
 import React from "react";
+
+const paths = ["products", "dao", "build", "docs"];
 
 export default function Navbar() {
   // @ts-ignore
-  const { logoutUser } = useAuth();
   return (
-    <div className=" fixed top-0 w-full flex items-center justify-between px-12 py-3 text-white">
-      <h1>Hello</h1>
-      <button className=" text-white" onClick={logoutUser}>
-        logoutUser
-      </button>
+    <div className=" mt- py-5 px-10 bg-black bg-opacity-70 backdrop-blur-md rounded-full border border-[#ffffff2e] w-10/12 mx-auto flex items-center justify-between text-white">
+      <Image src={logo} alt="logo" />
+      <div className=" flex gap-x-8">
+        {paths.map((path, idx) => (
+          <div className=" cursor-pointer hover:underline" key={idx}>{path}</div>
+        ))}
+      </div>
+      <PrimaryBtn title="launch app" />
     </div>
   );
 }
